6b2f0c4dde2aa14518144c102566b980e904d319
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2021-05-10 10:28:49 -0400

    Adding download feature

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DirectoryTreeService } from '../../services/directory-tree/directory-tree.service';
-import { ElectronService } from '../../core/services/electron/electron.service';
-import { Subscription } from 'rxjs';
-import { finalize } from 'rxjs/operators';
-import { AuthService } from '../../services/auth/auth.service';
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { DirectoryTreeService } from "../../services/directory-tree/directory-tree.service";
+import { ElectronService } from "../../core/services/electron/electron.service";
+import { Subscription } from "rxjs";
+import { AuthService } from "../../services/auth/auth.service";
+import { StorageService } from "../../services/storage/storage.service";
+import { MatSelectionListChange } from "@angular/material/list";
+import { Reference } from "@angular/fire/storage/interfaces";
+import { subscribeOn } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   auth: any;
@@ -25,17 +27,28 @@ export class HomeComponent implements OnInit, OnDestroy {
   uploadCanceled: boolean;
   getFinalizedStatusSub: Subscription;
   getUploadMsgSub: Subscription;
+  projectsFolders: Array<Reference>;
+  selectedFolder: string;
+  filesToDownload: Array<string>;
+  downloadPercentage: Array<number>;
+  isDownloading: boolean;
+  downloadSubscriptions: Array<Subscription>;
 
   constructor(
     private router: Router,
     private authService: AuthService,
     private electronService: ElectronService,
-    private directoryTreeService: DirectoryTreeService
+    private directoryTreeService: DirectoryTreeService,
+    private storageService: StorageService
   ) {
     this.auth = this.authService.getAuth();
-    this.auth.authState.subscribe(authState => {
-      if (!authState) this.initialize();
-    })
+    this.auth.authState.subscribe((authState) => {
+      if (authState) {
+        this.getProjectFolders();
+      } else {
+        this.initialize();
+      }
+    });
     this.fs = this.electronService.fs;
     this.path = this.electronService.path;
     this.dialog = this.electronService.remote.dialog;
@@ -50,25 +63,44 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.uploadPercentage = {};
     this.uploadFinalized = false;
     this.uploadCanceled = false;
-    this.uploadStatusMsg = '';
+    this.uploadStatusMsg = "";
     this.directoryTreeService.initialize();
+    this.projectsFolders = new Array<Reference>();
+    this.selectedFolder = "";
+    this.filesToDownload = Array<string>();
+    this.downloadPercentage = Array<number>();
+    this.storageService.downloadProgress.subscribe((file) => {
+      const index = this.filesToDownload.findIndex(value => value == file.fileName);
+      if (index == -1) {
+        this.downloadPercentage.push(file.downloadPercentage);
+        this.filesToDownload.push(file.fileName);
+      } else {
+        this.downloadPercentage[index] = file.downloadPercentage;
+      }
+    });
+    this.isDownloading = false;
+    this.downloadSubscriptions = new Array<Subscription>();
   }
 
   onFolderSelect() {
     this.initialize();
 
-    this.dialog.showOpenDialog({
-      title: 'Select folder to upload',
-      buttonLabel: 'Select',
-      properties: ['openDirectory']
-    }).then(directory => {
-      if (!directory.canceled) {
-        let directoryPath = directory.filePaths[0];
-        this.directoryTree = this.directoryTreeService.setDirectoryPath(directoryPath);
-        this.fileNames = this.directoryTreeService.getFileNames();
-        console.log("directoryTree :", this.directoryTree);
-      }
-    })
+    this.dialog
+      .showOpenDialog({
+        title: "Select folder to upload",
+        buttonLabel: "Select",
+        properties: ["openDirectory"],
+      })
+      .then((directory) => {
+        if (!directory.canceled) {
+          let directoryPath = directory.filePaths[0];
+          this.directoryTree = this.directoryTreeService.setDirectoryPath(
+            directoryPath
+          );
+          this.fileNames = this.directoryTreeService.getFileNames();
+          console.log("directoryTree :", this.directoryTree);
+        }
+      });
   }
 
   onUploadBtnClick() {
@@ -79,18 +111,51 @@ export class HomeComponent implements OnInit, OnDestroy {
     let uploadStatus = this.directoryTreeService.getUploadStatus();
 
     let uploadFinalized$ = uploadStatus.uploadFinalized;
-    this.getFinalizedStatusSub = uploadFinalized$.subscribe(uploadFinalized => {
-      this.uploadFinalized = uploadFinalized
-    });
+    this.getFinalizedStatusSub = uploadFinalized$.subscribe(
+      (uploadFinalized) => {
+        this.uploadFinalized = uploadFinalized;
+      }
+    );
 
     let uploadStatusMsg$ = uploadStatus.uploadStatusMsg;
-    this.getUploadMsgSub = uploadStatusMsg$.subscribe(uploadStatusMsg => {
-      this.uploadStatusMsg = uploadStatusMsg
+    this.getUploadMsgSub = uploadStatusMsg$.subscribe((uploadStatusMsg) => {
+      this.uploadStatusMsg = uploadStatusMsg;
     });
   }
 
   onCancelBtnClick() {
     this.uploadCanceled = this.directoryTreeService.cancelUpload();
+  }
+
+  getProjectFolders(): void {
+    let getFoldersSubscription = this.storageService.getFolders().subscribe(
+      (result) => {
+        this.projectsFolders = result.prefixes;
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        console.log("Download folders successfully completed!");
+      }
+    );
+    this.downloadSubscriptions.push(getFoldersSubscription)
+  }
+
+  onSelectionChange(event: MatSelectionListChange): void {
+    this.selectedFolder = event.options[0].value;
+  }
+
+  onDownload(): void {
+    this.filesToDownload.length = 0;
+    this.downloadPercentage.length = 0;
+    this.isDownloading = true;
+    const folderToDownload = this.projectsFolders.find(folder => folder.name == this.selectedFolder);
+    this.storageService.getFiles(folderToDownload);
+  }
+
+  onPrevious(): void {
+    this.isDownloading = false;
   }
 
   ngOnDestroy() {
@@ -101,5 +166,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.getUploadMsgSub) {
       this.getUploadMsgSub.unsubscribe();
     }
+
+    this.downloadSubscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -1,23 +1,77 @@
-import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/storage';
-
+import { Injectable } from "@angular/core";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { ListResult, Reference } from "@angular/fire/storage/interfaces";
+import { Net, Remote } from "electron";
+import { Observable, Subject } from "rxjs";
+import { AppConfig } from "../../../environments/environment";
+import { ElectronService } from "../../core/services/electron/electron.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StorageService {
-  storageRef;
+  downloadProgress: Subject<{ fileName: string, downloadPercentage: number }>;
 
-  constructor(
-    private storage: AngularFireStorage,
-  ) { }
+  constructor(private storage: AngularFireStorage, private electronService: ElectronService) {
+    this.downloadProgress = new Subject<{ fileName: string, downloadPercentage: number }>();
+  }
 
   /**
-  * Uploader un fichier sur storage
-  * @param filePath 
-  * @param file 
-  */
+   * Uploader un fichier sur storage
+   * @param filePath
+   * @param file
+   */
   uploadFile(filePath, file, extension) {
-    return this.storage.upload(filePath, file, {contentType : extension})
+    return this.storage.upload(filePath, file, { contentType: extension });
+  }
+
+  getFolders(): Observable<ListResult> {
+    return this.storage
+      .refFromURL(`gs://${AppConfig.firebase.storageBucket}`)
+      .listAll();
+  }
+
+  getFiles(folder: Reference): void {
+    folder.listAll()
+      .then((result) => {
+        result.items.forEach((item) => {
+          this.downloadProgress.next({ fileName: item.name, downloadPercentage: 0 })
+          item.getDownloadURL()
+            .then(url => this.downloadFile(url, item.name))
+            .catch(error => console.log(error))
+        })
+
+        result.prefixes.forEach((prefixe) => {
+          this.getFiles(prefixe)
+        })
+      })
+      .catch(error => console.log(error));
+  }
+
+  downloadFile(file_url: string, fileName: string): void {
+    let received_bytes = 0;
+    let total_bytes = 0;
+
+    let req = this.electronService.request({
+      method: 'GET',
+      uri: file_url
+    });
+
+    let out = this.electronService.fs.createWriteStream(`/Users/elheche/Downloads/Test/${fileName}`);
+    req.pipe(out);
+
+    req.on('response', (data) => {
+      total_bytes = parseInt(data.headers['content-length']);
+    });
+
+    req.on('data', (chunk) => {
+      received_bytes += chunk.length;
+      this.downloadProgress.next({ fileName: fileName, downloadPercentage: (received_bytes * 100) / total_bytes });
+      console.log((received_bytes * 100) / total_bytes + "% | " + received_bytes + " bytes out of " + total_bytes + " bytes.");
+    });
+
+    req.on('end', () => {
+      console.log("File succesfully downloaded");
+    });
   }
 }
