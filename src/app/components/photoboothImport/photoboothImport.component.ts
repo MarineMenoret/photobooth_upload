06eee1367011f406d8af12802a98912ada32937f
elheche <lamraoui.hichem@yahoo.ca>
2021-05-19 12:02:49 -0400

    Update photoboothImport.component

--- src/app/components/photoboothImport/photoboothImport.component.ts
+++ src/app/components/photoboothImport/photoboothImport.component.ts
@@ -1,60 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { ElectronService } from "../../core/services/electron/electron.service";
+import {Component} from "@angular/core";
+import {IDirectoryTree} from "../../shared/interfaces/directory-tree";
+import {IProject} from "../../shared/interfaces/project";
+
+const ELEMENT_DATA: IProject[] = [
+  { name: 'Project 1', creationDate: new Date(), directoryTree: {} as IDirectoryTree, files: [] },
+  { name: 'Project 2', creationDate: new Date(), directoryTree: {} as IDirectoryTree, files: [] },
+  { name: 'Project 2', creationDate: new Date(), directoryTree: {} as IDirectoryTree, files: [] },
+];
 
 @Component({
   selector: "photoboothImport",
   templateUrl: "./photoboothImport.component.html",
   styleUrls: ["./photoboothImport.component.scss"],
 })
-export class PhotoboothImportComponent implements OnInit {
-  fs: any;
-  path: any;
-  dialog: any;
-  projectsList: Array<string>;
-  displayedColumns: Array<string>;
-
-  constructor(private electronService: ElectronService) {
-    this.fs = this.electronService.fs;
-    this.path = this.electronService.path;
-    this.dialog = this.electronService.remote.dialog;
-    this.displayedColumns = ["Projects", "Local", "Distant", "actions-col"];
-
-    this.initialize();
-  }
-
-  initialize() {
-    this.projectsList = [];
-  }
-
-  ngOnInit(): void {}
-
-  onSelectFolderBtnClick() {
-    this.initialize();
-    this.dialog
-      .showOpenDialog({
-        title: "Select folder to import into photobooth",
-        buttonLabel: "Select",
-        properties: ["openDirectory"],
-      })
-      .then((directory) => {
-        if (!directory.canceled) {
-          const directoryPath = directory.filePaths[0];
-          const directoryChild = this.fs.readdirSync(directoryPath);
-
-          directoryChild.forEach(child => {
-            const childPath = this.path.join(directoryPath, child);
-            if (this.fs.lstatSync(childPath).isDirectory()) {
-              this.projectsList.push(child);
-            } 
-          });
-
-          
-        }
-      });
-  }
-
-  onImportBtnClick() {
-    console.log("import button clicked");
-    //appeler album
-  }
+export class PhotoboothImportComponent {
+  displayedColumns: string[] = ['project', 'creation date', 'state'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/shared/interfaces/project.ts
+++ src/app/shared/interfaces/project.ts
@@ -1,8 +1,9 @@
-import { IFile } from "./file";
+import {IDirectoryTree} from "./directory-tree";
+import {IFile} from "./file";
 
 export interface IProject {
-    name: string;
-    creationDate: Date;
-    directoryTree: any;
-    files: Array<IFile>;
+  name: string;
+  creationDate: Date;
+  directoryTree: IDirectoryTree;
+  files: Array<IFile>;
 }
