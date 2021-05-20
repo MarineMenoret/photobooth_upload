19d494258a15ef74f7bd11536e3b9f713f6e24b2
elheche <lamraoui.hichem@yahoo.ca>
2021-05-20 08:18:34 -0400

    Add getLocalProjects()

--- src/app/components/photoboothImport/photoboothImport.component.ts
+++ src/app/components/photoboothImport/photoboothImport.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy, OnInit} from "@angular/core";
 import {IProject} from "../../shared/interfaces/project";
 import {SyncService} from "../../services/sync/sync.service";
 import {Subscription} from "rxjs";
+import {ElectronService} from "../../core/services";
+import {IDirectoryTree} from "../../shared/interfaces/directory-tree";
 
 @Component({
   selector: "photoboothImport",
@@ -12,8 +14,9 @@ export class PhotoboothImportComponent implements OnInit, OnDestroy {
   subscriptions: Array<Subscription>;
   displayedColumns: Array<string>;
   remoteProjects: Array<IProject>;
+  localProjects: Array<IProject>;
 
-  constructor(private syncService: SyncService) {
+  constructor(private syncService: SyncService, private electronService: ElectronService) {
   }
 
   ngOnInit(): void {
@@ -23,7 +26,8 @@ export class PhotoboothImportComponent implements OnInit, OnDestroy {
   initialize(): void {
     this.subscriptions = new Array<Subscription>();
     this.displayedColumns = ['project', 'creation date', 'state'];
-    this.remoteProjects =  new Array<IProject>();
+    this.remoteProjects = new Array<IProject>();
+    this.localProjects = new Array<IProject>();
 
     this.subscriptions.push(
       this.syncService.remoteProjects$.subscribe((projects) => {
@@ -34,6 +38,37 @@ export class PhotoboothImportComponent implements OnInit, OnDestroy {
 
   getRemoteProjects(): void {
     this.syncService.getRemoteProjects();
+  }
+
+  getLocalProjects(): void {
+    const projects = new Array<IProject>();
+
+    this.electronService.remote.dialog.showOpenDialog({
+      title: "Select a folder to import projects into photobooth",
+      buttonLabel: "Select",
+      properties: ["openDirectory"],
+    })
+      .then((directory) => {
+        if (!directory.canceled) {
+          const directoryPath = directory.filePaths[0];
+          const directoryChild = this.electronService.fs.readdirSync(directoryPath);
+
+          directoryChild.forEach(child => {
+            const childPath = this.electronService.path.join(directoryPath, child);
+            if (this.electronService.fs.lstatSync(childPath).isDirectory()) {
+              const project: IProject = {
+                name: child,
+                creationDate: this.electronService.fs.lstatSync(childPath).birthtime,
+                directoryTree: {} as IDirectoryTree,
+                files: []
+              };
+              projects.push(project);
+            }
+          });
+          this.localProjects = projects;
+        }
+      })
+      .catch(error => console.log((error)));
   }
 
   ngOnDestroy(): void {
