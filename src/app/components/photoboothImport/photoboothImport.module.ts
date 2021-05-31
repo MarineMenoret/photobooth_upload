af3d9d86e3c078437aee6db461077195439e2c14
elheche <lamraoui.hichem@yahoo.ca>
2021-05-31 12:53:54 -0400

    Update photoboothImport.module.ts

--- src/app/components/photoboothImport/photoboothImport.module.ts
+++ src/app/components/photoboothImport/photoboothImport.module.ts
@@ -2,27 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {MaterialModule} from '../../material-module';
-import {MatTableModule} from '@angular/material/table';
 
 import {PhotoboothImportComponent} from './photoboothImport.component';
 import {SharedModule} from '../../shared/shared.module';
 
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [PhotoboothImportComponent],
   imports: [
     MaterialModule,
-    MatTableModule,
     CommonModule,
     SharedModule,
     BrowserAnimationsModule,
-    MatTooltipModule,
-    MatProgressSpinnerModule,
-    MatSnackBarModule
   ],
   exports: [
     PhotoboothImportComponent
