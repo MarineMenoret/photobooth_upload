02fa6c075aa0eb50b8bd29bf9e4b7d4dd86a81ba
elheche <lamraoui.hichem@yahoo.ca>
2021-05-31 12:54:34 -0400

    Minor changes

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MaterialModule } from '../../material-module';
+import {MaterialModule} from '../../material-module';
 
-import { HomeRoutingModule } from './home-routing.module';
+import {HomeRoutingModule} from './home-routing.module';
 
-import { HomeComponent } from './home.component';
-import { SharedModule } from '../../shared/shared.module';
+import {HomeComponent} from './home.component';
+import {SharedModule} from '../../shared/shared.module';
 
-import { HeaderModule } from '../header/header.module'
-import { PhotoboothImportModule } from '../photoboothImport/photoboothImport.module'
+import {HeaderModule} from '../header/header.module';
+import {PhotoboothImportModule} from '../photoboothImport/photoboothImport.module';
 
-import { AppConfig } from '../../../environments/environment';
-import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {AppConfig} from '../../../environments/environment';
+import {NgxAuthFirebaseUIModule} from 'ngx-auth-firebaseui';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {SyncDialogModule} from "../sync-dialog/sync-dialog.module";
 
 @NgModule({
   declarations: [HomeComponent],
@@ -22,12 +23,14 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MaterialModule,
     HeaderModule,
     PhotoboothImportModule,
-    CommonModule, 
-    SharedModule, 
-    HomeRoutingModule,  
+    SyncDialogModule,
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
     NgxAuthFirebaseUIModule.forRoot(AppConfig.firebase),
     MatListModule,
     MatProgressBarModule
   ]
 })
-export class HomeModule {}
+export class HomeModule {
+}
