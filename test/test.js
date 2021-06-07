5509777b6248595ffb7823cd0dff3d2902644427
Florence Cimon-Paquet <florencecimonpaquet@hotmail.com>
2021-06-07 15:19:29 -0400

    Add rules for projects and users doc access

--- test/test.js
+++ test/test.js
@@ -4,33 +4,35 @@ const firebase = require('@firebase/testing');
 const MY_PROJECT_ID = "angular-electron-5f77c";
 const myId = "user_abc";
 const theirId = "user_xyz";
-const modId = "user_mod";
+const adminId = "user_admin";
 const myAuth = {uid: myId, email: "abc@test.com"};
-const modAuth = {uid: modId, email: "mod@test.com", isModerator: true};
+const theirAuth = {uid: theirId, email: "@test.com"};
+const adminAuth = {uid: adminId, email: "admin@test.com"};
 
 function getFirestore(auth){
-    return firebase.initializeTestApp({projectId: MY_PROJECT_ID, auth: myAuth}).firestore();
+    return firebase.initializeTestApp({projectId: MY_PROJECT_ID, auth: auth}).firestore();
 }
 
-function getAdminFirestore(auth){
+function getAdminFirestore(){
     return firebase.initializeAdminApp({projectId: MY_PROJECT_ID}).firestore();
 }
 
-beforeEach (async() => {
+before (async() => {
     await firebase.clearFirestoreData({projectId: MY_PROJECT_ID});
 });
 
 describe("Photobooth upload", () => {
-    it("Can read items in the read-only collection", async() => {
-        const db = getFirestore(null);
-        const testDoc = db.collection("readonly").doc("testDoc");
-        await firebase.assertSucceeds(testDoc.get());
-    });
-
+    
     it("Can write to a user document with the same ID as our user", async() => {
         const db = getFirestore(myAuth);
         const testDoc = db.collection("users").doc(myId);
         await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
+    });
+
+    it("Admin role can write to a user document with the same ID as its own user", async() => {
+        const db = getFirestore(adminAuth);
+        const testDoc1 = db.collection("users").doc(adminId);
+        await firebase.assertSucceeds(testDoc1.set({foo: "bar"}));
     });
 
     it("Can't write to a user document with a different ID as our user", async() => {
@@ -39,106 +41,144 @@ describe("Photobooth upload", () => {
         await firebase.assertFails(testDoc.set({foo: "bar"}));
     });
 
-    it("Can read posts marked public", async() => {
+    it("Can't write to a user admin document with a different ID as our user", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("users").doc(adminId);
+        await firebase.assertFails(testDoc.set({foo: "bar"}));
+    });
+
+    it("Can't write to a user document if not logged in", async() => {
         const db = getFirestore(null);
-        const testQuery = db.collection("posts").where("visibility", "==", "public");
+        const testDoc = db.collection("users").doc(theirId);
+        await firebase.assertFails(testDoc.set({foo: "bar"}));
+    });
+
+    it("Can query personal projects", async() => {
+        const db = getFirestore(myAuth);
+        const testQuery = db.collection("projects").where("authorId", "==", myId);
         await firebase.assertSucceeds(testQuery.get());
     });
 
-    it("Can query personal posts", async() => {
+    it("Can't query all projects", async() => {
         const db = getFirestore(myAuth);
-        const testQuery = db.collection("posts").where("authorId", "==", myId);
-        await firebase.assertSucceeds(testQuery.get());
-    });
-
-    it("Can't query all posts", async() => {
-        const db = getFirestore(myAuth);
-        const testQuery = db.collection("posts");
+        const testQuery = db.collection("projects");
         await firebase.assertFails(testQuery.get());
     });
 
-    it("Can read a single public post", async() => {
-        const admin = getAdminFirestore();
-        const postId = "public_post";
-        const setupDoc = admin.collection("posts").doc(postId);
-        await setupDoc.set({authorId: theirId, visibility: "public"});
-        
+    it("Can't query all projects if not logged in", async() => {
         const db = getFirestore(null);
-        const testRead = db.collection("posts").doc(postId);
-        await firebase.assertSucceeds(testRead.get());
-    });
-    
-    it("Can read a private post belonging to the user", async() => {
-        const admin = getAdminFirestore();
-        const postId = "private_post";
-        const setupDoc = admin.collection("posts").doc(postId);
-        await setupDoc.set({authorId: myId, visibility: "private"});
-        
-        const db = getFirestore(myAuth);
-        const testRead = db.collection("posts").doc(postId);
-        await firebase.assertSucceeds(testRead.get());
+        const testQuery = db.collection("projects");
+        await firebase.assertFails(testQuery.get());
     });
 
-    it("Can't read a private post belonging to another user", async() => {
+    it("Can't read a private project belonging to another user", async() => {
         const admin = getAdminFirestore();
-        const postId = "private_post";
-        const setupDoc = admin.collection("posts").doc(postId);
+        const projectId = "private_project";
+        const setupDoc = admin.collection("projects").doc(projectId);
         await setupDoc.set({authorId: theirId, visibility: "private"});
         
         const db = getFirestore(myAuth);
-        const testRead = db.collection("posts").doc(postId);
+        const testRead = db.collection("projects").doc(projectId);
         await firebase.assertFails(testRead.get());
     });
 
-    it("Allows a user to edit their own post", async() => {
-        const postId = "post123";
+    it("Doesn't allow a user to edit somebody else's projects", async() => {
+        const projectId = "project123";
         const admin = getAdminFirestore();
-        await admin.collection("posts").doc(postId).set({content: "before", authorId: myId});
+        await admin.collection("projects").doc(projectId).set({content: "before", authorId: theirId});
 
         const db = getFirestore(myAuth);
-        const testDoc = db.collection("posts").doc(postId);
-        await firebase.assertSucceeds(testDoc.update({content: "after"}));
-
-    });
-
-    it("Doesn't allow a user to edit somebody else's post", async() => {
-        const postId = "post123";
-        const admin = getAdminFirestore();
-        await admin.collection("posts").doc(postId).set({content: "before", authorId: theirId});
-
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection("posts").doc(postId);
+        const testDoc = db.collection("projects").doc(projectId);
         await firebase.assertFails(testDoc.update({content: "after"}));
-
     });
 
-    // it("Allows moderator to edit somebody else's post", async() => {
-    //     const postId = "post123";
-    //     const admin = getAdminFirestore();
-    //     await admin.collection("posts").doc(postId).set({content: "before", authorId: theirId});
+    it("Doesn't allow a non-logged in user to edit somebody else's projects", async() => {
+        const projectId = "project123";
+        const admin = getAdminFirestore();
+        await admin.collection("projects").doc(projectId).set({content: "before", authorId: theirId});
 
-    //     const db = getFirestore(modAuth);
-    //     const testDoc = db.collection("posts").doc(postId);
-    //     await firebase.assertSucceeds(testDoc.update({content: "after"}));
-    // });
-
-    it("Allows a user to create a post when they list themselves as the author", async() => {
-        const postPath = "/posts/post_123";
-        const db = getFirestore(myAuth);
-        const testDoc = db.doc(postPath);
-        await firebase.assertSucceeds(testDoc.set({authorId: myId, content: "lorem ipsum"}));
+        const db = getFirestore(null);
+        const testDoc = db.collection("projects").doc(projectId);
+        await firebase.assertFails(testDoc.update({content: "not logged in"}));
     });
 
-    it("Doesn't allow a user to create a post when they list somebody else as the author", async() => {
-        const postPath = "/posts/post_123";
+    it("Allows admin to edit its own project", async() => {
+        const projectId = "projectadmin";
+        const admin = getAdminFirestore();
+        await admin.collection("projects").doc(projectId).set({content: "before", authorId: adminId});
+
+        const db = getFirestore(adminAuth);
+        const testDoc = db.collection("projects").doc(projectId);
+        await firebase.assertSucceeds(testDoc.update({content: "after"}));
+    });
+
+    it("Doesn't allow a user to create a project when they list somebody else as the author", async() => {
+        const projectPath = "/projects/project_123";
         const db = getFirestore(myAuth);
-        const testDoc = db.doc(postPath);
+        const testDoc = db.doc(projectPath);
         await firebase.assertFails(testDoc.set({authorId: theirId, content: "lorem ipsum"}));
+    });
 
+    it("Can't list all projects belonging to another user", async() => {
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("projects").where("authorId", "==", theirId);
+        await firebase.assertFails(testRead.get());
+    });
+
+    it("Can't list all projects", async() => {
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("projects");
+        await firebase.assertFails(testRead.get());
+    });
+
+    it("Super admin can create an admin role user", async() => {
+        const db = getAdminFirestore();
+        const testDoc = db.collection("admins").doc(adminId);
+        await firebase.assertSucceeds(testDoc.set({content: "before"}));
+    });
+
+    it("Admin can't create an admin role user", async() => {
+        const db = getFirestore(adminAuth);
+        const testDoc = db.collection("admins").doc(myId);
+        await firebase.assertFails(testDoc.set({content: "before"}));
+    });
+
+    it("Can't create an admin role user", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("admins").doc(myId);
+        await firebase.assertFails(testDoc.set({content: "before"}));
+    });
+
+    it("Can't create an admin role user if not logged in", async() => {
+        const db = getFirestore(null);
+        const testDoc = db.collection("admins").doc(myId);
+        await firebase.assertFails(testDoc.set({content: "before"}));
+    });
+
+    it("Admin role can write to a user document with a different ID as our user", async() => {
+        const db = getFirestore(adminAuth);
+        const testDoc = db.collection("users").doc(myId);
+        await firebase.assertSucceeds(testDoc.set({foo: "after"}));
+    });
+
+    it("Allows admin to edit somebody else's project", async() => {
+        const projectId = "project_abc";
+        const admin = getAdminFirestore();
+        await admin.collection("projects").doc(projectId).set({content: "before", authorId: myId});
+
+        const db = getFirestore(adminAuth);
+        const testDoc = db.collection("projects").doc(projectId);
+        await firebase.assertSucceeds(testDoc.update({content: "after"}));
+    });
+
+    it("Can query all projects if is an admin", async() => {
+        const db = getFirestore(adminAuth);
+        const testQuery = db.collection("projects");
+        await firebase.assertSucceeds(testQuery.get());
     });
 
 });
 
-after(async() => {
-    await firebase.clearFirestoreData({projectId: MY_PROJECT_ID});
-});
+// after(async() => {
+//     await firebase.clearFirestoreData({projectId: MY_PROJECT_ID});
+// });
